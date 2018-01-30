f567cccf0d99c0fb4e969bccc8e5751d26518b42
Dale <outofthisworld24@hotmail.com>
2018-01-30 03:12:17 -0100

    c

--- src/task/WorldTask.java
+++ src/task/WorldTask.java
@@ -56,26 +56,86 @@
 package task;
 
 public abstract class WorldTask implements Task {
-    private final int numTicks;
-    private final int initialDelayTicks;
-    private final int repeatedDelayTicks;
-    private final int repeatTimes;
-    private final int numTicksCount;
 
-    public WorldTask(int numTicks, int initialDelayTicks, int repeatedDelayTicks, int repeatTimes) {
-        this.numTicks = numTicks;
+    /* The number of times this task should execute */
+    private final int numExecutions;
+    /*
+        The successive number of ticks between each execution of this task
+    */
+    private final int repeatedDelayTicks;
+    /*
+        The number of ticks before this task first executes
+    */
+    private final int initialDelayTicks;
+    /*
+        If the initial delay should be
+     */
+    private final boolean repeatInitialDelay;
+    /*
+       The number of times this task should repeat
+
+       One time is the whole cycle e.g initial delay - execute n times - sucessive delay - execute n times
+    */
+    private int repeatTimes;
+    private int numExecutionsCount;
+    private int repeatedDelayCount;
+    private int initialDelayCount;
+
+    public WorldTask(int numExecutions, int initialDelayTicks, int repeatedDelayTicks, int repeatTimes, boolean repeatInitialDelay) {
+        this.numExecutions = numExecutions;
         this.initialDelayTicks = initialDelayTicks;
         this.repeatedDelayTicks = repeatedDelayTicks;
         this.repeatTimes = repeatTimes;
-        numTicksCount = this.numTicks;
+        this.repeatInitialDelay = repeatInitialDelay;
+        numExecutionsCount = numExecutions;
+        repeatedDelayCount = repeatedDelayTicks;
+        initialDelayCount = initialDelayTicks;
     }
-
 
     @Override
     public boolean check() {
 
+        //Wait until the initial delay has finished
+        if (initialDelayCount > 0) {
+            initialDelayCount--;
+            return false;
+        }
 
-        return true;
+
+        if (initialDelayCount == 0 && numExecutionsCount > 0) {
+            numExecutionsCount--;
+            return true;
+        }
+
+        if (initialDelayCount == 0 && numExecutionsCount == 0) {
+            numExecutionsCount = numExecutions;
+        }
+
+        if (repeatedDelayCount > 0) {
+            repeatedDelayCount--;
+            return false;
+        }
+
+        if (repeatedDelayCount == 0 && numExecutionsCount > 0) {
+            numExecutionsCount--;
+            return true;
+        }
+
+        if (repeatedDelayCount == 0 && numExecutionsCount == 0) {
+            if (repeatTimes > 0) {
+                repeatTimes--;
+            }
+            if (repeatInitialDelay) {
+                initialDelayCount = initialDelayTicks;
+            }
+            numExecutionsCount = numExecutions;
+            repeatedDelayCount = repeatedDelayTicks;
+            if (repeatTimes != 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
